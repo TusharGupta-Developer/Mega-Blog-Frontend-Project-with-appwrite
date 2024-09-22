e7d756c6a9dfa1ef81da47de0a595f512e655e67
TusharGupta-Developer <tushar.developer127@gmail.com>
2024-09-22 10:28:15 +0530

    Updated

--- src/componenets/Header/LogoutBtn.jsx
+++ src/componenets/Header/LogoutBtn.jsx
@@ -6,7 +6,7 @@ import { logout } from '../../store/authSlice'
 function LogoutBtn() {
     const dispatch = useDispatch()
     const LogoutHandler = () => {
-        authService.logout().then(() => {
+        authService.logout().then(() => { // as async function logout in auth.js does not use return, but it used with async so it will return promise but value will be undefined. So how to check properly that session is deleted in logout function in auth.js ????????????
             dispatch(logout())
         })
     }
--- src/componenets/index.js
+++ src/componenets/index.js
@@ -1,4 +1,6 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from "./Container/Container";
+import Logo from "./logo";
 
-export {Header, Footer}
+export { Footer, Container, Header, Logo}
