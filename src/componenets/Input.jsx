7e99cd2c76235d7e7ecc66cfc2f4a8c47b96929b
TusharGupta-Developer <tushar.developer127@gmail.com>
2024-09-22 10:24:08 +0530

    Maked common Input field Component

---
+++ src/componenets/Input.jsx
@@ -0,0 +1,30 @@
+import React, { useId } from 'react'
+
+const Input = React.forwardRef(function Input({
+    label,
+    type = "rext",
+    className = '',
+    ...props
+}, ref) {
+
+    const id = useId()
+    return (
+        <div className="w-full">
+            {/* if label is present */}
+            {label && <label
+                className='block mb-1'>
+                {label}
+            </label>}
+
+            <input type={type}
+                className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+                ref={ref}
+                {...props}
+                id={id}
+            />
+            {/* id of label and input will be same so whenever click on label then focus will go to its input field */}
+        </div>
+    )
+})
+
+export default Input
