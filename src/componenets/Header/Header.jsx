f9b56c4d8e29373642e0a4245deab884437a3589
TusharGupta-Developer <tushar.developer127@gmail.com>
2024-09-22 10:19:43 +0530

    Maked NAVBar in Header Component

--- src/componenets/Header/Header.jsx
+++ src/componenets/Header/Header.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { container, Logo, LogoutBtn } from "../index"
-import { Link } from 'react-router-dom'
+import { Link, Navigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
+import Container from './../Container/Container';
+import Logo from './../logo';
+import LogoutBtn from './LogoutBtn';
 
 function Header() {
-  const authStatus = useSelector((state) => {
-    state.auth.status
-  })
+  const authStatus = useSelector((state) => state.auth.status)
+  const Navigate = useNavigate()
+
 
   const navItems = [
     {
@@ -40,7 +43,40 @@ function Header() {
 
 
   return (
-    <div>Header</div>
+    <haeder className="py-3 shadow bg-gray-500">
+      <Container>
+        <nav>
+          <div className="flex"></div>
+          <div className="mr-4">
+            <Link to='/'>
+
+              <Logo width='70px' />
+
+            </Link>
+          </div>
+          <ul className='flex ml-auto'>
+            {navItems.map((item) =>
+              item.active ? (
+                // Unordered list
+                <li key={item.name}>  {/* Providing a unique key for each item to help React identify elements */}
+                  <button onClick={() => Navigate(item.slug)}
+                    className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'>
+                    {item.name}
+                  </button>
+                </li> // Where HTML Element repeat then there is use of key.
+              ) : null
+            )}
+
+            {/* if authStatus is true then show the user logout Button */}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
+          </ul>
+        </nav>
+      </Container>
+    </haeder>
   )
 }
 
