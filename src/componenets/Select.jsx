3200e74bf8570a5c7bf875308ddcc91cac873c1c
TusharGupta-Developer <tushar.developer127@gmail.com>
2024-09-23 09:39:55 +0530

    Debugged the Select Component

--- src/componenets/Select.jsx
+++ src/componenets/Select.jsx
@@ -13,9 +13,9 @@ function Select({
             {label && <label className='' htmlFor={id}></label>}
             <select {...props} id='id' ref={ref} className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
             >
-                {options?.map((option) =>( //if options have then only map should happen
+                {options?.map((option) => ( //if options have then only map should happen
                     <option key={option} value={option}>
-                        {options}
+                        {option}
                     </option>
                 ))}
             </select>
