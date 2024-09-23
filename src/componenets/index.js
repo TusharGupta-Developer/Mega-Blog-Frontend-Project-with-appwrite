4cf8b489fd7987139c35b5063c3c7da69d12a161
TusharGupta-Developer <tushar.developer127@gmail.com>
2024-09-23 14:16:07 +0530

    Creating Login form using 'react-hook-form'

--- src/componenets/index.js
+++ src/componenets/index.js
@@ -2,5 +2,10 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
 import Logo from "./logo";
+import Input from "./Input";
+import Select from "./Select";
+import PostCard from './PostCard';
+import Button from './Button';
 
-export { Footer, Container, Header, Logo}
+
+export { Footer, Container, Header, Logo, PostCard, Button, Input, Select}
